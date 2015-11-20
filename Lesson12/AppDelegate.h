7af8ec8b33b6815f981c4515b2109180ab471c1b
Azat Almeev <azat.almeev@gmail.com>
2015-11-20 17:11:06 +0300

    Initial Commit

---
+++ Lesson12/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson12
+//
+//  Created by Azat Almeev on 20.11.15.
+//  Copyright © 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson12/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson12
+//
+//  Created by Azat Almeev on 20.11.15.
+//  Copyright © 2015 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
